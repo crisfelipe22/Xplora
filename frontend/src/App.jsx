a0fa316036dded1ba6592039b16f2deba0d01dc7
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-02-14 00:42:57 -0300

    fix: Agregar ruta /admin y corregir estructura del Router en App.jsx
    - Se añade la ruta `/admin` con el componente `Admin`.
    - Se agrega una ruta alternativa `/home` que también renderiza `Home`.
    - Se corrige la estructura del Router, eliminando `<Router>` dentro de `App.jsx`.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home"; // Página principal
+import Admin from "./components/AddProductForm"; // Página principal
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
