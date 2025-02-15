6ed761071980fd7e4d675fddd9a253225564f285
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-02-15 14:53:41 -0300

    feat: Reemplazar clases globales por módulos CSS en el componente Header
    + Se cambió la importación de `header.css` por `header.module.css` en el componente Header.jsx.
    + Se actualizaron las clases CSS a la sintaxis de módulos, utilizando `className={styles.clase}`.
    + Se renombraron las clases de la sección izquierda del header (`header__left` a `header__brand`) para mantener la coherencia con los módulos CSS.
    + Se optimizó la forma de aplicar clases múltiples a un solo elemento usando la sintaxis de plantillas literales.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from '@mui/material/Button';
-import "../styles/header.css"; 
+import styles from "../styles/header.module.css"; 
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="header__left">
-        <Link to="/" className="header__logo">
-          <div className="header__logo">
+    <header className={styles.header}>
+      <div className={styles.header__brand}>
+        <Link to="/" className={styles.header__logo}>
+          <div className={styles.header__logo}>
             <img src="/logo.svg"></img>
           </div>
-          <span className="header__slogan">XPLORA</span>
+          <span className={styles.header__slogan}>
+            XPLORA</span>
         </Link>
       </div>
-      <div className="header__right">
-        <Button variant="text" className="header__button">
+      <div className={styles.header__right}>
+        <Button variant="text" className={styles.header__button}>
           CREAR CUENTA
           </Button>
-        <Button variant="contained" color="primary" className="header__button header__button--login">
+        <Button variant="contained" color="primary" className={`${styles.header__button} ${styles["header__button--login"]}`}>
           INICIAR SESIÓN
         </Button>
       </div>
