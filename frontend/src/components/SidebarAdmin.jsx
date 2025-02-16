2362c30efb79ecef4095d47b1aff1233a1eb182c
SaraCarol <mendoza.saracarolina@gmail.com>
2025-02-16 15:26:01 -0500

    style: Estilos del panel de administración

--- frontend/src/components/SidebarAdmin.jsx
+++ frontend/src/components/SidebarAdmin.jsx
@@ -8,25 +8,49 @@ import { Link } from 'react-router-dom';
 const SidebarAdmin = () => {
 
     return (
-        <Drawer variant="permanent" className={styles.sidebar}>
+        <Box className={styles.panel}>
 
-            <List>
-                <Link to='/admin' >
-                    <ListItemButton >
-                        <ListItemIcon><DashboardIcon /></ListItemIcon>
-                        <ListItemText primary="Inicio" />
-                    </ListItemButton>
-                </Link>
-                    
-                <Link to="/admin/productos">
-                    <ListItemButton >
-                        <ListItemIcon><AddIcon /></ListItemIcon>
-                        <ListItemText primary="Lista de productos" />
-                    </ListItemButton>
-                </Link>
-                    
-            </List>
-        </Drawer>
+            <Box className={styles.header}>
+                <Typography variant="h6" className={styles.panelTitle}>
+                    Panel de Administración
+                </Typography>
+            </Box>
+
+            <Box className={styles.sidebarBox}>
+                <Drawer variant="permanent" className={styles.sidebar}
+                    sx={{
+                        '& .MuiDrawer-paper': {
+                            position: 'relative',
+                            width: 240,
+                            height: 'calc(100vh - 64px)',
+                            boxSizing: 'border-box'
+                        }
+                    }}>
+                    <List className={styles.menuList} > 
+                        <Link to='/admin' style={{ textDecoration: 'none' }} >
+                            <ListItemButton className={styles.menuItem} >
+                                <ListItemIcon>
+                                    <DashboardIcon className={styles.menuIcon}/>
+                                </ListItemIcon>
+                                <ListItemText primary="Inicio" className={styles.menuText} />
+                            </ListItemButton>
+                        </Link>
+                            
+                        <Link to="/admin/productos" style={{ textDecoration: 'none' }}>
+                            <ListItemButton className={styles.menuItem}>
+                                <ListItemIcon>
+                                    <AddIcon className={styles.menuIcon}/>
+                                </ListItemIcon>
+                                <ListItemText primary="Lista de productos" className={styles.menuText} />
+                            </ListItemButton>
+                        </Link>
+                            
+                    </List>
+                </Drawer>
+            </Box>
+
+        </Box>
+        
         
     );
 };
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -2,7 +2,6 @@
 // eslint-disable-next-line no-unused-vars
 import AddProductForm from "../components/AddProductForm"
 import AdminPanel from "../components/AdminPanel"
-
 import SidebarAdmin from "../components/SidebarAdmin"
 
 const Admin = () =>{
