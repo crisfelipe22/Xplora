607397b488641460008619e55ba5e12d3ddc3ab8
crisfelipe22 <cris_ovalles@hotmail.com>
2025-02-15 23:42:17 -0500

    [ADD] Dto, service and controller for experience package

---
+++ src/main/java/com/backend/service/impl/IPaqueteExperiencia.java
@@ -0,0 +1,8 @@
+package com.backend.service.impl;
+
+import com.backend.dto.salida.Categoria.PaqueteExperienciaSalidaDto;
+
+public class IPaqueteExperiencia {
+
+    PaqueteExperienciaSalidaDto registrarPaqueteExperiencia
+}
