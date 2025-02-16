ce1698defd1d03401a2e51de804e1d7a5caaa810
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-02-16 20:36:25 -0300

    feat: Modifica archivos de back para habilitar comunicación entre Springboot y React
    + También trae el ouput npm run build y lo sirve como un recurso estático

--- src/main/java/com/backend/XploraApplication.java
+++ src/main/java/com/backend/XploraApplication.java
@@ -1,5 +1,8 @@
 package com.backend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import com.backend.dbconnection.H2Connection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class XploraApplication {
     public static void main(String[] args){
-        // Ejecuta el script de inicialización de la base de datos
         H2Connection.ejecutarScriptInicial();
 
         // Inicia la aplicación Spring Boot
