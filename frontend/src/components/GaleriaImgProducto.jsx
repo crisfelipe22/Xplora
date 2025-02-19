2036828bed5aafe612358b780eac68eac72c3ea1
SaraCarol <mendoza.saracarolina@gmail.com>
2025-02-19 16:58:35 -0500

    front-style: realizando los estilos de la vista detalle producto

--- frontend/src/components/GaleriaImgProducto.jsx
+++ frontend/src/components/GaleriaImgProducto.jsx
@@ -1,16 +1,11 @@
 import styles from "../styles/GaleriaImgProducto.module.css";
 import { Dialog, DialogActions, DialogContent, Button } from "@mui/material";
-import { useState } from "react";
+/* eslint-disable react/prop-types */
 
-const GaleriaImgProducto = () => {
-
-    //ESTAS FUNCIONES DEBEN ESTAR EN DETAIL Y PASARSE POR PARAMETRO OPENGALLERY Y HANDLECLOSEGALLERY
-    const [openGallery, setOpenGallery] = useState(true);
-    //const handleOpenGallery = () => setOpenGallery(true);
-    const handleCloseGallery = () => setOpenGallery(false);
+const GaleriaImgProducto = ({ open, close, imagen }) => {
 
     //simulando las imagenes por ahora
-    const imagen = "https://i.pinimg.com/736x/d6/c9/42/d6c94291ddccad51cc91da0f0ebc7b53.jpg, https://i.pinimg.com/736x/0b/7b/20/0b7b20a4891cfdce18cabe2b22236e0c.jpg, https://i.pinimg.com/736x/77/fc/72/77fc72bd2d4c422f649db61b59d4e509.jpg, https://i.pinimg.com/736x/1e/1a/3a/1e1a3a3f20f5acbd2022bf0fc62880e9.jpg, https://i.pinimg.com/736x/95/9b/0b/959b0b3115c3b804a42c64f9ea7148e8.jpg, https://i.pinimg.com/736x/4a/a1/1c/4aa11c37114ca51952c01370b28d19aa.jpg, https://i.pinimg.com/736x/b6/23/62/b6236212dbae1d18701e5a1debb5a4db.jpg"
+    //const imagen = "https://i.pinimg.com/736x/d6/c9/42/d6c94291ddccad51cc91da0f0ebc7b53.jpg, https://i.pinimg.com/736x/0b/7b/20/0b7b20a4891cfdce18cabe2b22236e0c.jpg, https://i.pinimg.com/736x/77/fc/72/77fc72bd2d4c422f649db61b59d4e509.jpg, https://i.pinimg.com/736x/1e/1a/3a/1e1a3a3f20f5acbd2022bf0fc62880e9.jpg, https://i.pinimg.com/736x/95/9b/0b/959b0b3115c3b804a42c64f9ea7148e8.jpg, https://i.pinimg.com/736x/4a/a1/1c/4aa11c37114ca51952c01370b28d19aa.jpg, https://i.pinimg.com/736x/b6/23/62/b6236212dbae1d18701e5a1debb5a4db.jpg"
 
     const imagenArray = imagen ? imagen.split(',').map(url => url.trim()) : [];
 
@@ -20,7 +15,7 @@ const GaleriaImgProducto = () => {
     console.log(imagenArray[0])
 
     return (
-        <Dialog open={openGallery} onClose={handleCloseGallery}>
+        <Dialog open={open} onClose={close}>
             <DialogContent>
                 <div className={styles.galeria}>
                     {imagenArray.map((img, index) =>(
@@ -32,7 +27,7 @@ const GaleriaImgProducto = () => {
             </DialogContent>
 
             <DialogActions>
-                <Button color="primary" onClick={handleCloseGallery}>
+                <Button color="primary" onClick={close}>
                     Cerrar
                 </Button>
             </DialogActions>
