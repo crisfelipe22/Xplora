700e82c363218366fb4912e227ba253a805dee3c
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-02-20 02:17:51 -0300

    feat: Redirigir rutas sin extensión y manejar errores o refrescos de página con rutas anidadas
    + Se implementa la interfaz ErrorController para manejar rutas de error.
    + Se actualiza la lógica para redirigir todas las rutas sin extensión de archivo al index.html.
    + Se agrega un método para manejar errores y redirigir a index.html en caso de fallo.
    + Se reemplaza el uso de @GetMapping por @RequestMapping para abarcar más tipos de solicitudes.

--- src/main/java/com/backend/controller/WebController.java
+++ src/main/java/com/backend/controller/WebController.java
@@ -1,12 +1,20 @@
 package com.backend.controller;
-
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class WebController {
-    @GetMapping("/{path:[^\\.]*}")
-    public String forward() {
+public class WebController implements ErrorController {
+    
+    // Redirige todas las rutas que no tienen extensión de archivo al index
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+    
+    // Maneja las rutas de error redirigiendo al index
+    @RequestMapping(value = "/error")
+    public String handleError() {
         return "forward:/index.html";
     }
 }
