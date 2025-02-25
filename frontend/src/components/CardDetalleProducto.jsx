37e7427ef8f1fa162ca4a762b466c473ce9e9b8e
SaraCarol <mendoza.saracarolina@gmail.com>
2025-02-24 20:14:50 -0500

    Front-refactor modificando comentarios del código

--- frontend/src/components/CardDetalleProducto.jsx
+++ frontend/src/components/CardDetalleProducto.jsx
@@ -36,7 +36,7 @@ const CardDetalleProducto = ({product}) =>{
         <Container className={styles.container}>
             <div className={styles.detalleSuperior}>
                 <div className={styles.tituloVolver}>
-                    <IconButton component={Link} to="/admin/productos" className={styles.backButton}>
+                    <IconButton component={Link} to="/" className={styles.backButton}>
                         <ArrowBackIcon /> VOLVER ATRÁS
                     </IconButton>
                     <Typography variant="h3" className={styles.title}>{product.nombre}</Typography>
@@ -56,7 +56,7 @@ const CardDetalleProducto = ({product}) =>{
             <Box className={styles.contenedorDetalles}> 
                 <Box className={styles.contenedorDos}>
                     <div className={styles.seccionRating}>
-                        {/* <Chip label={product.categoria.nombre} className={styles.chip} /> */}
+                        {<Chip label={product.categoria.nombre} className={styles.chip} />}
                         <Rating value={rating} precision={0.5} readOnly className={styles.rating} />
                     </div>    
                     <List dense>
