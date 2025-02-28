33e7da5bb875cd14575bac105869ec2e366ce050
Andreina <peraza.andreina02@gmail.com>
2025-02-27 22:45:30 -0300

    feat: agregando pagina de registro

---
+++ frontend/src/pages/Registro.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FormRegistroUsuario from "../components/FormRegistroUsuario";
+
+const Registro = () => {
+  return (
+    <>
+      <FormRegistroUsuario />
+    </>
+  );
+};
+
+export default Registro;
