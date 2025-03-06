78fc4b41d6753b93243ea8da41d063704748656f
SaraCarol <mendoza.saracarolina@gmail.com>
2025-03-05 20:20:49 -0500

    Merge con pre release 2.0

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
