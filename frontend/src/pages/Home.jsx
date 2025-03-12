495a6718afbe668fa01a510f2c11176768c767a2
SaraCarol <mendoza.saracarolina@gmail.com>
2025-03-11 21:16:10 -0500

    front-style: Estilos de los textField del home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,6 @@ import { BeachAccess, CalendarToday } from "@mui/icons-material";
 /*import { DateRangePicker } from "@mui/x-date-pickers";*/
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import dayjs from "dayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import es from "date-fns/locale/es";
@@ -170,6 +169,8 @@ const Home = () => {
                   inline
                   locale={es}
                   monthsShown={2}
+                  direction="horizontal"
+                  rangeColors={["#7B1FA2"]}
                 />
               )}
 
