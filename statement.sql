55cffbd603c386c81331c5478e2c05ad281c0cb5
Solymar Quiaro <solymarquiaro@gmail.com>
2025-03-14 10:17:11 -0500

    [Backend-update]: se eliminó de paquete de experiencia el campo fecha_experiencia y se crearon fecha_inicio y fecha_fin

--- statement.sql
+++ statement.sql
@@ -3,7 +3,12 @@ use xplora_db;
 INSERT INTO categoria (nombre) VALUES
     ('Aventuras y deportes'),
     ('Gastronomia'),
-    ('Estadias');
+    ('Estadias'),
+    ('Bienestar y relajación'),
+    ('Cultura y entretenimiento'),
+    ('Naturaleza y ecoturismo'),
+    ('Experiencias urbanas'),
+    ('Romanticismo y arte');
 
 INSERT INTO rol (id_rol, nombre) VALUES
     (1, 'SuperAdministrador'),
@@ -15,19 +20,31 @@ INSERT INTO usuario (id_usuario, nombre, direccion, contrasena, telefono, id_rol
     (2, 'Xplora Admin', 'Xplora Boulevard 123', '$2a$10$d8celupi88HCw5E07LAXeu9MBYKPDBpiNtgPxCvhstDN4CyXdSWxi', 12345678,2,'admin@xplora.com'),
     (3, 'Xplora Usuario', 'Xplora Boulevard 123', '$2a$10$bixazDGCVrXkoCtSkrzkle3uCyzUCHvkC3a0aYkZWxSBN5HBlbhLa', 12345678,3,'usuario@xplora.com');
 
-INSERT INTO paquete_experiencia (id_paquete_experiencia, nombre, descripcion, precio, ubicacion, imagen, duracion, fecha_experiencia, id_categoria) VALUES
-    (1, 'Spa de Lujo', 'Aventura extrema en aguas rápidas', 150.00, 'Mendoza, Argentina', '/imagen_1.jpeg,/imagen_1.jpeg,/imagen_1.jpeg,/imagen_1.jpeg,/imagen_1.jpeg', '4 horas', NOW(), 1),
-    (2, 'Paseo en kayak', 'Navega en kaya frente a uno de los glaciares más famosos', 120.00, 'Buenos Aires, Argentina', '/imagen_2.jpeg,/imagen_3.jpeg,/imagen_4.jpeg,/imagen_5.jpeg,/imagen_6.jpeg', '3 horas', NOW(), 2),
-    (3, 'Noche en cabaña', 'Escapada romántica en la montaña', 200.00, 'Bariloche, Argentina', '/imagen_8.jpeg', '1 noche', NOW(),  3),
-    (4, 'Parapente en la montaña', 'Vuela sobre los Andes y disfruta de vistas panorámicas', 150.00, 'Mendoza, Argentina', '/imagen_88.jpeg', '4 horas', NOW(), 1),
-    (5, 'Tour de vinos premium', 'Cata exclusiva en viñedos históricos', 120.00, 'Buenos Aires, Argentina', '/imagen_9.jpeg', '3 horas', NOW(), 2),
-    (6, 'Aventura en glamping', 'Noche de lujo en una tienda bajo las estrellas', 200.00, 'Bariloche, Argentina', '/imagen_10.jpeg', '1 noche', NOW(), 3),
-    (7, 'Rafting en aguas blancas', 'Descenso extremo por rápidos clase III y IV', 150.00, 'Mendoza, Argentina', '/imagen_11.jpeg', '4 horas', NOW(), 1),
-    (8, 'Experiencia gastronómica fusión', 'Menú degustación con maridaje en restaurante de autor', 120.00, 'Buenos Aires, Argentina', '/imagen_12.jpeg', '3 horas', NOW(), 2),
-    (9, 'Refugio alpino exclusivo', 'Hospedaje en cabaña con chimenea y vistas al lago', 200.00, 'Bariloche, Argentina', '/imagen_13.jpeg', '1 noche', NOW(), 3),
-    (10, 'Buceo en aguas cristalinas', 'Explora arrecifes y vida marina en un entorno único', 150.00, 'Mendoza, Argentina', '/imagen_14.jpeg', '4 horas', NOW(), 1),
-    (11, 'Taller de cocina internacional', 'Aprende técnicas de chefs expertos en un entorno exclusivo', 120.00, 'Buenos Aires, Argentina', '/imagen_15.jpeg', '3 horas', NOW(), 2),
-    (12, 'Campamento en la nieve', 'Vive la aventura de dormir en la montaña nevada', 200.00, 'Bariloche, Argentina', '/imagen_16.jpeg', '1 noche', NOW(), 3);
+INSERT INTO paquete_experiencia (id_paquete_experiencia, nombre, descripcion, precio, ubicacion, imagen, duracion, fecha_inicio, fecha_fin, id_categoria) VALUES
+    (1, 'Spa de Lujo', 'Aventura extrema en aguas rápidas', 150.00, 'Mendoza, Argentina', '/imagen_1.jpeg,/imagen_1.jpeg,/imagen_1.jpeg,/imagen_1.jpeg,/imagen_1.jpeg', '4 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 1),
+    (2, 'Paseo en kayak', 'Navega en kaya frente a uno de los glaciares más famosos', 120.00, 'Buenos Aires, Argentina', '/imagen_2.jpeg,/imagen_3.jpeg,/imagen_4.jpeg,/imagen_5.jpeg,/imagen_6.jpeg', '3 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 2),
+    (3, 'Noche en cabaña', 'Escapada romántica en la montaña', 200.00, 'Bariloche, Argentina', '/imagen_8.jpeg', '1 noche',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 3),
+    (4, 'Parapente en la montaña', 'Vuela sobre los Andes y disfruta de vistas panorámicas', 150.00, 'Mendoza, Argentina', '/imagen_88.jpeg', '4 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 1),
+    (5, 'Tour de vinos premium', 'Cata exclusiva en viñedos históricos', 120.00, 'Buenos Aires, Argentina', '/imagen_9.jpeg', '3 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 2),
+    (6, 'Aventura en glamping', 'Noche de lujo en una tienda bajo las estrellas', 200.00, 'Bariloche, Argentina', '/imagen_10.jpeg', '1 noche',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 3),
+    (7, 'Rafting en aguas blancas', 'Descenso extremo por rápidos clase III y IV', 150.00, 'Mendoza, Argentina', '/imagen_11.jpeg', '4 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 1),
+    (8, 'Experiencia gastronómica fusión', 'Menú degustación con maridaje en restaurante de autor', 120.00, 'Buenos Aires, Argentina', '/imagen_12.jpeg', '3 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 2),
+    (9, 'Refugio alpino exclusivo', 'Hospedaje en cabaña con chimenea y vistas al lago', 200.00, 'Bariloche, Argentina', '/imagen_13.jpeg', '1 noche',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 3),
+    (10, 'Buceo en aguas cristalinas', 'Explora arrecifes y vida marina en un entorno único', 150.00, 'Mendoza, Argentina', '/imagen_14.jpeg', '4 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 1),
+    (11, 'Taller de cocina internacional', 'Aprende técnicas de chefs expertos en un entorno exclusivo', 120.00, 'Buenos Aires, Argentina', '/imagen_15.jpeg', '3 horas',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 2),
+    (12, 'Campamento en la nieve', 'Vive la aventura de dormir en la montaña nevada', 200.00, 'Bariloche, Argentina', '/imagen_16.jpeg', '1 noche',
+        @fecha_inicio := NOW() + INTERVAL FLOOR(RAND() * 7 + 1) DAY, @fecha_inicio + INTERVAL FLOOR(RAND() * 7 + 1) DAY, 3);
 
 
 INSERT INTO pedido (estado, es_regalo, total, id_paquete_experiencia) VALUES
