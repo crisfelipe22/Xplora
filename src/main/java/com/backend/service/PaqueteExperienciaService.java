f62f0d6dc2b367cdf4884c6c49db2d1c1b4a13fe
vicrioss <vikerios14@gmail.com>
2025-03-15 17:51:13 -0300

    Guardando cambios antes del merge

--- src/main/java/com/backend/service/PaqueteExperienciaService.java
+++ src/main/java/com/backend/service/PaqueteExperienciaService.java
@@ -126,19 +126,29 @@ public class PaqueteExperienciaService {
         return paqueteExperienciaSalidaDto;
     }
 
-    public List<PaqueteExperienciaSalidaDTO> obtenerPaqueteExperienciaPorFiltro(String nombre, Date fecha_inicio, Date fecha_fin) {
-        try {
-            logger.info("Obteniendo datos del paquete por filtro: nombre={}, fecha_inicio={}, fecha_fin={}", nombre, fecha_inicio, fecha_fin);
-
-            List<PaqueteExperiencia> paquetes = paqueteExperienciaRepository.findByFilter(nombre, fecha_inicio, fecha_fin);
-            return paquetes.stream()
-                    .map(paquete -> modelMapper.map(paquete, PaqueteExperienciaSalidaDTO.class))
-                    .collect(Collectors.toList());
-        } catch (Exception e) {
-            logger.error("Error obteniendo paquetes por filtro", e);
-            throw new RuntimeException("Error obteniendo paquetes por filtro");
+    public List<PaqueteExperienciaSalidaDTO> obtenerPaqueteExperienciaPorFiltro(String nombre, Date fecha_inicio, Date fecha_fin, Long categoriaId) {
+    try {
+        logger.info("Obteniendo datos del paquete por filtro: nombre={}, fecha_inicio={}, fecha_fin={}, categoriaId={}", nombre, fecha_inicio, fecha_fin, categoriaId);
+        List<PaqueteExperiencia> paquetes;
+        if (categoriaId != null) {
+            // Si categoriaId no es null, filtrar por categoría, nombre y fechas
+            paquetes = paqueteExperienciaRepository.findByCategoriaIdAndNombreAndFechaExperienciaBetween(categoriaId, nombre, fecha_inicio, fecha_fin);
+        } else {
+            // Si no se especifica categoría, filtrar solo por nombre y fechas
+            paquetes = paqueteExperienciaRepository.findByNombreAndFechaExperienciaBetween(nombre, fecha_inicio, fecha_fin);
         }
+        return paquetes.stream()
+                .map(paquete -> {
+                    PaqueteExperienciaSalidaDTO dto = modelMapper.map(paquete, PaqueteExperienciaSalidaDTO.class);
+                    dto.setId_categoria(paquete.getCategoria().getId_categoria());
+                    return dto;
+                })
+                .collect(Collectors.toList());
+    } catch (Exception e) {
+        logger.error("Error obteniendo paquetes por filtro", e);
+        throw new RuntimeException("Error obteniendo paquetes por filtro");
     }
+}
 
     @Transactional
     public PaqueteExperienciaSalidaDTO eliminarPaqueteExperiencia(Long id) throws ResourceNotFoundException, BadRequestException {
