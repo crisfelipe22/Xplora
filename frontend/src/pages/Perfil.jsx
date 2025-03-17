9555c964a47be86c541d36337b830b624349dafd
Victoria Ancalaf <victoria.ancalaf@gmail.com>
2025-03-16 21:14:14 -0300

    style user contact information

--- frontend/src/pages/Perfil.jsx
+++ frontend/src/pages/Perfil.jsx
@@ -1,29 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';
-import { 
-  Container, 
-  Paper, 
-  Typography, 
-  Box, 
-  Grid, 
-  Avatar, 
-  Divider, 
-  Card, 
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Paper,
+  Typography,
+  Box,
+  Avatar,
+  Card,
   CardContent,
-  useTheme,
-  useMediaQuery, 
   Tabs,
   Tab,
-  IconButton
-} from '@mui/material';
+  IconButton,
+} from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
-import HomeIcon from '@mui/icons-material/Home';
-import PhoneIcon from '@mui/icons-material/Phone';
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import { AuthContext } from '../contexts/AuthContext'; 
+import { AuthContext } from "../contexts/AuthContext";
 
 const obtenerRolTexto = (idRol) => {
   switch (idRol) {
@@ -46,7 +37,8 @@ const PerfilUsuario = () => {
   const [tabValue, setTabValue] = useState(0);
   const [favorites, setFavorites] = useState([]);
   const handleTabChange = (event, newValue) => {
-    setTabValue(newValue)};
+    setTabValue(newValue);
+  };
 
   // Cargar favoritos desde localStorage al montar el componente
   useEffect(() => {
@@ -60,18 +52,19 @@ const PerfilUsuario = () => {
   }, [favorites]);
 
   // Agregar un producto a favoritos
-  const handleAddFavorite = (product) => {
+  /* const handleAddFavorite = (product) => {
     if (!favorites.some((fav) => fav.id === product.id)) {
       setFavorites([...favorites, product]);
     }
-  };
+  }; */
 
   // Eliminar un producto de favoritos
   const removeFavorite = (productId) => {
-    const updatedFavorites = favorites.filter((product) => product.id !== productId);
+    const updatedFavorites = favorites.filter(
+      (product) => product.id !== productId
+    );
     setFavorites(updatedFavorites);
   };
-
 
   useEffect(() => {
     if (!user || !user.id) {
@@ -121,146 +114,160 @@ const PerfilUsuario = () => {
 
   return (
     <div>
-    
-    <Container maxWidth="md" sx={{ marginTop: "75px" }}>
-      <Paper 
-        elevation={3} 
-        sx={{ 
-          p: { mobile: 2, tablet: 4 },
-          borderRadius: 2,
-          overflow: 'hidden'
-        }}
-      >
-        <Box 
-          sx={{ 
-            bgcolor: 'primary.main', 
-            p: 3, 
-            mb: 4, 
-            borderRadius: '8px 8px 0 0',
-            margin: -4,
-            marginBottom: 4,
+      <Container maxWidth="md" sx={{ marginTop: "113px", mb: 4 }}>
+        <Paper
+          elevation={3}
+          sx={{
+            p: { mobile: 2, tablet: 4 },
+            borderRadius: 2,
+            overflow: "hidden",
+            height: "calc(100vh - 80px)",
           }}
         >
-          <Typography 
-            variant="h4" 
-            component="h1" 
-            sx={{ 
-              color: 'white', 
-              fontWeight: 'bold',
-              textAlign: 'center'
+          <Box
+            sx={{
+              p: 2,
             }}
           >
-            Mi Cuenta
-          </Typography>
-        </Box>
-
-        <Box sx={{ width: "100%", typography: "body1", marginTop: 5 }}>
-      <Tabs value={tabValue} onChange={handleTabChange} aria-label="account tabs">
-        <Tab icon={<AccountCircleIcon />} label="PERFIL" />
-        <Tab icon={<FavoriteIcon />} label="LISTA DE FAVORITOS" />
-      </Tabs>
-      {tabValue === 0 && (
-
-        <Box p={3}>
-          <Grid container spacing={4}>
-          <Grid item xs={12} md={4} sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: "20px" }}>
-              <Avatar 
-                sx={{ 
-                  width: 120, 
-                  height: 120, 
-                  bgcolor: 'secondary.main',
-                  fontSize: 40,
-                  mb: 2
-                }}
-              >
-                {obtenerIniciales(currentUser.nombre)}
-              </Avatar>
-              <Typography variant="h6" align="center" gutterBottom>
-                {currentUser.nombre}
-              </Typography>
-              <Typography 
-                variant="body2" 
-                align="center" 
-                sx={{ 
-                  bgcolor: 'secondary.main', 
-                  color: 'white', 
-                  px: 2, 
-                  py: 0.5, 
-                  borderRadius: 2 
-                }}
-              >
-                {obtenerRolTexto(currentUser.id_rol)}
-              </Typography>
-            </Box>
-          </Grid>
-
-          <Grid item xs={12} md={8}>
-            <Typography variant="h6" sx={{ mb: 2, fontWeight: 'bold' }}>
-              Información Personal
+            <Typography
+              sx={{ typography: { mobile: "h5", tablet: "h4", desktop: "h4" } }}
+            >
+              Mi Cuenta
             </Typography>
-            <Divider sx={{ mb: 3 }} />
-            
-            <Grid container spacing={2}>
-              {infoItems.map((item, index) => (
-                <Grid item xs={12} key={index}>
-                  <Card 
-                    variant="outlined" 
-                    sx={{ 
-                      mb: 1,
-                      '&:hover': {
-                        boxShadow: 2,
-                        borderColor: 'secondary.light'
-                      }
+          </Box>
+
+          <Box sx={{ width: "100%", typography: "body1" }}>
+            <Tabs
+              variant="scrollable"
+              scrollButtons
+              allowScrollButtonsMobile
+              value={tabValue}
+              onChange={handleTabChange}
+              aria-label="account tabs"
+            >
+              <Tab icon={<AccountCircleIcon />} label="PERFIL" />
+              <Tab icon={<FavoriteIcon />} label="LISTA DE FAVORITOS" />
+            </Tabs>
+            {tabValue === 0 && (
+              <Card
+                sx={{
+                  backgroundColor: "background.default",
+                  mt: 2,
+                  width: "100%",
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    px: 2.5,
+                    py: 2,
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      typography: {
+                        mobile: "h6",
+                        tablet: "h5",
+                        desktop: "h5",
+                      },
                     }}
                   >
-                    <CardContent sx={{ p: 2, '&:last-child': { pb: 2 } }}>
-                      <Grid container alignItems="center" spacing={1}>
-                        <Grid item>
-                          <Box sx={{ color: 'secondary.main', display: 'flex' }}>
-                            {item.icon}
-                          </Box>
-                        </Grid>
-                        <Grid item xs>
-                          <Typography variant="body2" color="text.secondary">
-                            {item.label}:
-                          </Typography>
-                          <Typography variant="body1">
-                            {item.value}
-                          </Typography>
-                        </Grid>
-                      </Grid>
-                    </CardContent>
-                  </Card>
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-        </Grid>
-        </Box>
+                    {currentUser.nombre}
+                  </Typography>
 
-      )}
-      {tabValue === 1 && (
-        <Box p={3}>
-          {favorites.length === 0 ? (
-            <Typography>No tienes productos en tu lista de favoritos.</Typography>
-          ) : (
-            favorites.map((product) => (
-              <Box key={product.id} display="flex" alignItems="center" justifyContent="space-between" p={1} borderBottom="1px solid #ddd">
-                <Typography>{product.name}</Typography>
-                <IconButton onClick={() => removeFavorite(product.id)}>
-                  <FavoriteIcon color="error" />
-                </IconButton>
+                  <Avatar
+                    sx={{
+                      width: {
+                        mobile: "40px",
+                        tablet: "100px",
+                      },
+                      height: {
+                        mobile: "40px",
+                        tablet: "100px",
+                      },
+                      bgcolor: "secondary.main",
+                      fontSize: {
+                        tablet: "48px",
+                      },
+                    }}
+                  >
+                    {obtenerIniciales(currentUser.nombre)}
+                  </Avatar>
+                </Box>
+                <CardContent
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    px: 2.5,
+                    py: 2,
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      typography: { mobile: "body2", tablet: "subtitle1" },
+                    }}
+                  >
+                    Tipo Usuario
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    sx={{
+                      typography: { mobile: "body1", tablet: "h6" },
+                      fontWeight: { mobile: 600 },
+                      pb: 2,
+                    }}
+                  >
+                    {obtenerRolTexto(currentUser.id_rol)}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      typography: { mobile: "body2", tablet: "subtitle1" },
+                    }}
+                  >
+                    Correo electrónico
+                  </Typography>
+                  <Typography
+                    gutterBottom
+                    sx={{
+                      typography: { mobile: "body1", tablet: "h6" },
+                      fontWeight: { mobile: 600 },
+                    }}
+                  >
+                    {currentUser.email}
+                  </Typography>
+                </CardContent>
+              </Card>
+            )}
+            {tabValue === 1 && (
+              <Box p={3}>
+                {favorites.length === 0 ? (
+                  <Typography>
+                    No tienes productos en tu lista de favoritos.
+                  </Typography>
+                ) : (
+                  favorites.map((product) => (
+                    <Box
+                      key={product.id}
+                      display="flex"
+                      alignItems="center"
+                      justifyContent="space-between"
+                      p={1}
+                      borderBottom="1px solid #ddd"
+                    >
+                      <Typography>{product.name}</Typography>
+                      <IconButton onClick={() => removeFavorite(product.id)}>
+                        <FavoriteIcon color="error" />
+                      </IconButton>
+                    </Box>
+                  ))
+                )}
               </Box>
-            ))
-          )}
-        </Box>
-      )}
-    </Box>
-        
-      </Paper>
-    </Container>
-
+            )}
+          </Box>
+        </Paper>
+      </Container>
     </div>
   );
 };
