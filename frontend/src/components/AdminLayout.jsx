cc63cd9edcce890767abdd44a5176b9d6d3a312b
Luis-F-Cerda-P <luis.f.cerda.p@gmail.com>
2025-03-27 23:37:48 -0300

    feat: ajustes en componentes y estilos del panel de administración
    + Se actualizaron los componentes del panel de administración con nuevas importaciones y cambios en el diseño.
    + Se ajustaron los estilos en los formularios de características y productos, eliminando márgenes y ajustando tamaños de contenedores.
    + Se corrigieron detalles visuales en el SidebarAdmin y el AdminLayout, incluyendo la posición fija del menú y la eliminación de estilos redundantes.
    + Se agregaron nuevos iconos y elementos para mejorar la accesibilidad y funcionalidad de la interfaz de usuario.

--- frontend/src/components/AdminLayout.jsx
+++ frontend/src/components/AdminLayout.jsx
@@ -9,7 +9,7 @@ const AdminLayout = ({ children }) => {
         return (
             <Box 
                 sx={{ 
-                    height: "100vh", 
+                    // height: "100vh", 
                     display: "flex", 
                     alignItems: "center", 
                     justifyContent: "center",
