649f58c49a844a5b0b9921117206b78ce214c9ca
SaraCarol <mendoza.saracarolina@gmail.com>
2025-04-01 23:20:49 -0500

    front-style: Estilos de la seccion calificacion

--- frontend/src/pages/Calificaciones.jsx
+++ frontend/src/pages/Calificaciones.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 
-import { Typography} from "@mui/material";
+import { Typography, Box, Button} from "@mui/material";
 import CardCalificacionProducto from "../components/CardCalificacionProducto";
+import styles from "../styles/CardCalificacionProducto.module.css";
+import StarIcon from "@mui/icons-material/Star"
 
 const Calificaciones = () => {
 
@@ -18,21 +20,45 @@ const Calificaciones = () => {
             fecha_calificacion: '2024-11-01',
             calificacion: 3.0,
             descripcion: 'Simplemente mágico, empiezas el recorrido caminando, conoces dos cascadas increíbles, un poco de historia y pasas a conectarte con: La paz, la naturaleza, la conservación y la vida.'
+        },
+        {
+            usuario: 'Sara Mendoza',
+            fecha_calificacion: '2025-03-14',
+            calificacion: 4.5,
+            descripcion: 'Simplemente mágico, empiezas el recorrido caminando, conoces dos cascadas increíbles, un poco de historia y pasas a conectarte con: La paz, la naturaleza, la conservación y la vida.'
+        },
+        {
+            usuario: 'Solymar Quiaro',
+            fecha_calificacion: '2024-11-01',
+            calificacion: 3.0,
+            descripcion: 'Simplemente mágico, empiezas el recorrido caminando, conoces dos cascadas increíbles, un poco de historia y pasas a conectarte con: La paz, la naturaleza, la conservación y la vida.'
         }
     ]
 
     const promedioCalificacion = 3
 
+    
     return (
-        <div>
-            <Typography variant="h5">Reseñas</Typography>
-                {calificaciones.length > 0 ? (
-                    calificaciones.map((cal, index) => 
-                    <CardCalificacionProducto key={index} calificacion={cal} /> )
-                ) : (
+        <Box className={styles.calificacionesBox}>
+            <Typography variant="h5" className={styles.titulo} >Reseñas</Typography>
+            <Box className={styles.header}>
+                <Box display="flex" alignItems="center">
+                    <StarIcon style={{ color: "#FFC107", fontSize: "24px" }} /> 
+                    <Typography variant="body1" style={{ marginLeft: 8 }}>
+                        {promedioCalificacion} • {calificaciones.length} reseñas
+                    </Typography>
+                </Box>
+                <Button className={styles.botonEscribir}>Escribe tu reseña</Button>
+            </Box>
+
+            {calificaciones.length > 0 ? (
+                calificaciones.map((cal, index) => <CardCalificacionProducto key={index} calificacion={cal} />)
+            ) : (
                 <Typography variant="body1">Aún no hay reseñas.</Typography>
             )}
-        </div>
+
+            <Button variant="outlined" className={styles.botonVerTodas}>Ver todas las reseñas</Button>
+    </Box>
     );
 };
 
