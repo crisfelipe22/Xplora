871ed67c3b87e1992a0be4bf22af65f2589f00e1
crisfelipe22 <cris_ovalles@hotmail.com>
2025-04-02 19:31:35 -0500

    [Backend - refactor] se modifico el controlador

--- src/main/java/com/backend/controller/AuthController.java
+++ src/main/java/com/backend/controller/AuthController.java
@@ -2,15 +2,12 @@ package com.backend.controller;
 
 import com.backend.dto.salida.AuthResponseDTO;
 import com.backend.dto.entada.ConfirmationResendRequestDTO;
-import com.backend.dto.salida.CalificacionSalidaDTO;
-import com.backend.dto.entada.CalificacionEntradaDTO;
 import com.backend.dto.entada.LoginRequestDTO;
 import com.backend.dto.salida.MensajeResponseDTO;
 import com.backend.dto.salida.PaqueteExperienciaFavoritoSalidaDTO;
 import com.backend.dto.entada.RegistroRequestDTO;
 import com.backend.dto.salida.UsuarioSalidaDTO;
 import com.backend.service.AuthService;
-import com.backend.service.CalificacionService;
 
 import jakarta.validation.Valid;
 
@@ -33,9 +30,6 @@ public class AuthController {
 
     @Autowired
     private AuthService authService;
-    
-    @Autowired
-    private CalificacionService calificacionService;
 
     @PostMapping("/registro") public ResponseEntity<MensajeResponseDTO> registrarUsuario(@RequestBody @Valid RegistroRequestDTO registroDTO) throws ResourceNotFoundException {
       MensajeResponseDTO mensaje = authService.registrarUsuario(registroDTO);
@@ -142,42 +136,6 @@ public class AuthController {
     public ResponseEntity<MensajeResponseDTO> reenviarCorreo(@RequestBody ConfirmationResendRequestDTO confirmationResendRequestDTO) throws ResourceNotFoundException {
       MensajeResponseDTO mensaje = authService.reenviarCorreo(confirmationResendRequestDTO);
       return new ResponseEntity<>(mensaje, HttpStatus.OK);
-    }
-    @PostMapping("/{id_usuario}/reservas/{id_reserva}/calificaciones")
-    public ResponseEntity<CalificacionSalidaDTO> crearCalificacion(
-            @PathVariable(name = "id_usuario") Long id_usuario,
-            @PathVariable(name = "id_reserva") Long id_reserva,
-            @RequestBody CalificacionEntradaDTO calificacionDTO) throws ResourceNotFoundException, AccessDeniedException {
-        return new ResponseEntity<>(
-                calificacionService.crearCalificacion(id_usuario, id_reserva, calificacionDTO), HttpStatus.CREATED);
-    }
-
-    @GetMapping("/{id_usuario}/calificaciones/{id_calificacion}")
-    public ResponseEntity<CalificacionSalidaDTO> obtenerCalificacionPorId(
-            @PathVariable(name = "id_usuario") Long id_usuario, 
-            @PathVariable(name = "id_calificacion") Long id_calificacion) throws ResourceNotFoundException {
-        return ResponseEntity.ok(calificacionService.obtenerCalificacionPorId(id_usuario, id_calificacion));
-    }
-
-    @GetMapping("/{id_usuario}/calificaciones")
-    public ResponseEntity<List<CalificacionSalidaDTO>> obtenerTodasLasCalificaciones(
-            @PathVariable(name = "id_usuario") Long id_usuario) {
-        return ResponseEntity.ok(calificacionService.obtenerTodasLasCalificaciones(id_usuario));
-    }
-
-    @PutMapping("/{id_usuario}/calificaciones/{id_calificacion}")
-    public ResponseEntity<CalificacionSalidaDTO> actualizarCalificacion(
-            @PathVariable(name = "id_usuario") Long id_usuario,
-            @PathVariable(name = "id_calificacion") Long id_calificacion,
-            @RequestBody CalificacionEntradaDTO calificacionDTO) throws ResourceNotFoundException, AccessDeniedException {
-        return ResponseEntity.ok(calificacionService.actualizarCalificacion(id_usuario, id_calificacion, calificacionDTO));
-    }
-
-    @DeleteMapping("/{id_usuario}/calificaciones/{id_calificacion}")
-    public ResponseEntity<CalificacionSalidaDTO> eliminarCalificacion(
-            @PathVariable(name = "id_usuario") Long id_usuario,
-            @PathVariable(name = "id_calificacion") Long id_calificacion) throws ResourceNotFoundException, AccessDeniedException {
-        return ResponseEntity.ok(calificacionService.eliminarCalificacion(id_usuario, id_calificacion));
     }
 
 }
--- src/main/java/com/backend/security/config/SecurityConfig.java
+++ src/main/java/com/backend/security/config/SecurityConfig.java
@@ -89,6 +89,12 @@ public class SecurityConfig {
                             auth.requestMatchers(HttpMethod.GET, "/api/rol/**").permitAll();
                             auth.requestMatchers(HttpMethod.PUT, "/api/rol").hasAnyRole("Administrador", "SuperAdministrador");
                             auth.requestMatchers(HttpMethod.DELETE, "/api/rol").hasAnyRole("Administrador", "SuperAdministrador");
+                            auth.requestMatchers(HttpMethod.GET, "/api/calificaciones").permitAll();
+                            auth.requestMatchers(HttpMethod.GET, "/api/calificaciones/**").permitAll();
+                            auth.requestMatchers(HttpMethod.PUT, "/api/calificaciones").hasAnyRole("Administrador", "SuperAdministrador");
+                            auth.requestMatchers(HttpMethod.DELETE, "/api/calificaciones").hasAnyRole("Administrador", "SuperAdministrador");
+                            auth.requestMatchers(HttpMethod.POST, "/api/calificaciones").hasAnyRole("Administrador", "SuperAdministrador");
+                            
                             // auth.requestMatchers(HttpMethod.GET, "/api/whatsapp/link").permitAll();
                             
                             auth.requestMatchers("/**").permitAll()
